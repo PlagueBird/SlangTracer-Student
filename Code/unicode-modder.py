6db5a2345a3d18bfb5a59299dd73d809bee89285
PlagueBird <ccopley445@gmail.com>
2024-12-01 04:53:33 -0500

    finalization of html prepreprocessor

--- Code/unicode-modder.py
+++ Code/unicode-modder.py
@@ -2,21 +2,21 @@ import html
 
 def convert_to_unicode(input_file, output_file):
     try:
-        #Read
         with open(input_file, 'r', encoding='utf-8') as file:
             content = file.read()
-        
-        # 
-        content_blob = ""
-        for char in content:
-            char.replace("'", "\\xe2\\x80\\x99").replace("'", "\\xe2\\x80\\x98").replace("\n", "\\n")
+
+        # Replace the left and right apostrophes accordingly
+        content = content.replace("'", "\\xe2\\x80\\x99").replace('"', "\\xe2\\x80\\x9c").replace("\n", "\\n")
+
+        content = html.escape(content)
 
         with open(output_file, 'w', encoding='utf-8') as file:
-            file.write(content_blob)
+            file.write(content)
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
-input_html_file = '3e7vqxq.html'  #Replace
-output_html_file = '3e7vqxq.html' #Replace
+# Replacement Clause
+input_html_file = 'sausage.html'
+output_html_file = 'itmbtqq.html'
 convert_to_unicode(input_html_file, output_html_file)
